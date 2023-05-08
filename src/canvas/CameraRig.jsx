f8211a7ef32b13c91d7cab8c0ac4b3d2e1e59b85
THREEhundo <samuelbaik87@gmail.com>
2023-05-08 15:09:31 -0700

    3D canvas added

---
+++ src/canvas/CameraRig.jsx
@@ -0,0 +1,27 @@
+import { useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
+import { easing } from 'maath'
+import { useSnapshot } from 'valtio'
+
+import state from '../store'
+
+const CameraRig = ({ children }) => {
+	const group = useRef()
+	const snap = useSnapshot(state)
+
+	useFrame((state, delta) => {
+		// set the model rotation smoothly
+
+		easing.dampE(
+			group.current.rotation,
+			[state.pointer.y / 10, -state.pointer.x / 5, 0],
+			0.25,
+			delta
+		)
+	})
+
+	return <group ref={group}>{children}</group>
+}
+
+export default CameraRig
+// 1:00:27
